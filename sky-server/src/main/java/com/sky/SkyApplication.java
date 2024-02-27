94232c14e39e695df99e4451a3f46037206407fa
judys <jiangkai1110@gmail.com>
2024-02-27 15:34:27 +0800

    20240201 新增员工业务代码

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 public class SkyApplication {
+   /* 先启动nginx，位置在C:\demoProject\20240201_project_demo_1.0\nginx-1.20.2，桌面也有快捷方式*/
+   /* 前端数据封装成DTO对象->sql返回数据封装成entity对象->返回给前端数据封装成VO对象->返回格式result对象，*/
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
