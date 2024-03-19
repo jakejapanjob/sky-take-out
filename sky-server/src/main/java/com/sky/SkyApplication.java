14f29f0f0c7469eb8af66f434877fb2216c198e2
judys <jiangkai1110@gmail.com>
2024-03-19 18:08:44 +0800

    20240319 缓存菜品以及套餐管理功能

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -11,6 +11,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class SkyApplication {
    /* 先启动nginx，位置在C:\demoProject\20240201_project_demo_1.0\nginx-1.20.2，桌面也有快捷方式*/
    /* 前端数据封装成DTO对象->sql返回数据封装成entity对象->返回给前端数据封装成VO对象->返回格式result对象，*/
+
+    //AppID(小程序ID)	wx33fce485b4a13e5f
+    //db813cfd2420566d7da537029dbc4dbb
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,62 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.HttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+    @Autowired
+    private UserMapper userMapper;
+    public static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
+    @Override
+    public User wxLogin(UserLoginDTO userLoginDTO) {
+
+        String openid = getOpenid(userLoginDTO.getCode());
+
+        if(openid==null){
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+
+        User user = userMapper.getByOpenId(openid);
+        if(user == null){
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+        return user;
+    }
+
+    private String getOpenid(String code){
+        HashMap<String, String> paramMap = new HashMap<>();
+        paramMap.put("appid",weChatProperties.getAppid());
+        paramMap.put("secret",weChatProperties.getSecret());
+        paramMap.put("js_code",code);
+        paramMap.put("grant_type","authorization_code");
+        String json = HttpClientUtil.doGet(WX_LOGIN, paramMap);
+
+        JSONObject jsonObject = JSON.parseObject(json);
+        String openid = jsonObject.getString("openid");
+        return openid;
+    }
+}
